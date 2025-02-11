f3f21df2ff9594bc55d7f13af97bb25640537a30
Sakarias Näslund <sakarias.naslund@gmail.com>
2025-02-11 12:21:28 +0100

    Started booking system

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
 import { RouterProvider } from "react-router";
 import router from "./routes/Router";
 import "./styles/global.scss";
+import ShowBooking from "./components/ShowBooking";
+import { CreateBooking } from "./pages/CreateBooking";
 
 function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <>
+      <RouterProvider router={router} />
+      <CreateBooking />
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/ShowBooking.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ShowBooking = () => {
+  return <div>ShowBooking</div>;
+};
+
+export default ShowBooking;
