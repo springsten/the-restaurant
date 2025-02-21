eb75919611c17a2c3b399dbb548b85afe5eb3d2a
Sakarias Näslund <sakarias.naslund@gmail.com>
2025-02-21 10:14:30 +0100

    Small style fixes

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,21 +1,23 @@
-
-import React from "react";
 import { NavLink } from "react-router";
 
 const Hero = () => {
   return (
     <section className="hero">
       <div className="hero-content">
-        <h1>Små rätter, stora smaker</h1>
+        <h1>
+          Små rätter,
+          <br /> stora smaker
+        </h1>
         <p>
-          Upplev det bästa från det japanska köket – noggrant utvalda smårätter, 
+          Upplev det bästa från det japanska köket – noggrant utvalda smårätter,
           perfekt balanserade smaker.
         </p>
-        <NavLink to="/booking" className="hero-button">Boka bord</NavLink>
+        <NavLink to="/booking" className="hero-button">
+          Boka bord
+        </NavLink>
       </div>
     </section>
   );
 };
 
 export default Hero;
-
